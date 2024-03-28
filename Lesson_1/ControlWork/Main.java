5598e74acd4ff8326d8364db1e934a1acc8c9160
Damir Akhmetgaleev <artbox12345@mail.ru>
2024-03-28 21:25:55 +0500

    save42

---
+++ Lesson_1/ControlWork/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1. name = "Муззик";
+        cat1. age = 10;
+        cat1. owner = "Олег";
+        cat1. color = "Рыжий";
+        cat1. sterilization = false;
+
+        Cat cat2 = new Cat();
+        cat2. name = "Васька";
+        cat2. age = 9;
+        cat2. owner = "Петр";
+        cat2. color = "Белый";
+        cat2. sterilization = false;
+
+        Cat cat3 = new Cat();
+        cat3. name = "Толик";
+        cat3. age = 8;
+        cat3. owner = "Татьяна";
+        cat3. color = "Черный";
+        cat3. sterilization = true;
+
+        Cat cat4 = new Cat();
+        cat4. name = "Чижик";
+        cat4. age = 11;
+        cat4. owner = "Алина";
+        cat4. color = "Рыжий";
+        cat4. sterilization = true;
+
+        System.out.println(cat1);
+    }
+
+}
