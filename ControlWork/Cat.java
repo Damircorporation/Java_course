764d592fe56fdef34437672371f0a287753f00c0
Damir Akhmetgaleev <artbox12345@mail.ru>
2024-03-28 21:01:06 +0500

    save41

---
+++ ControlWork/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    int age;
+    String name;
+    String owner;
+    boolean sterilization;
+    String color;
+}
+
+public String toString(){
+    return "name :" + name + ", age :"+ age + ", owner :"+ owner + ", color :"+ color + ", sterilization :"+ sterilization; 
+}
