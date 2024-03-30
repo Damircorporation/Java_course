9d3b2f91f3eb07529785a6ecde3ac4cd08a94d31
Damir Akhmetgaleev <artbox12345@mail.ru>
2024-03-30 21:37:58 +0500

    save44

---
+++ Lesson_1/ControlWork/Laptop.java
@@ -0,0 +1,28 @@
+import java.util.Objects;
+
+public class Laptop {
+    int ram;
+    int storageSize;
+    String os;
+    String color;
+
+// Выводим всю инфу что имеем
+    public String toString(){
+        return "ram :" + ram + ", storageSize :"+ storageSize + ", os :"+ os + ", color :"+ color; 
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj instanceof Laptop laptop) {
+            return storageSize == laptop.storageSize && ram.equals(laptop.ram) && os.equals(laptop.os)&& color.equals(laptop.color);   
+        }
+        return false;
+    }
+    
+// сравниваем по ХэшКоду
+    public int hashCode(){ 
+        return storageSize+7*ram.hashCode()+11*os.hashCode()+17*color.hashCode();
+    }
+}
--- Lesson_1/ControlWork/Main.java
+++ Lesson_1/ControlWork/Main.java
@@ -1,61 +1,72 @@
+// Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
+// Создать множество ноутбуков.
+// Написать метод, который будет запрашивать у пользователя критерий (или критерии) фильтрации и выведет ноутбуки,
+// отвечающие фильтру. Критерии фильтрации можно хранить в Map.
+// Например:
+// “Введите цифру, соответствующую необходимому критерию:
+// 1 - ОЗУ
+// 2 - Объем ЖД
+// 3 - Операционная система
+// 4 - Цвет …
+// 5 - поиск
+// Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации можно
+// также в Map.
+// Отфильтровать ноутбуки из первоначального множества и вывести проходящие по условиям.
+
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat1 = new Cat();
-        cat1. name = "Мурзик";
-        cat1. age = 10;
-        cat1. owner = "Олег";
-        cat1. color = "Рыжий";
-        cat1. sterilization = false;
+        Laptop laptop1 = new Laptop();
+        laptop1. ram = 8;
+        laptop1. storageSize = 512;
+        laptop1. os = "Windows";
+        laptop1. color = "Silver";
 
-        Cat cat2 = new Cat();
-        cat2. name = "Васька";
-        cat2. age = 9;
-        cat2. owner = "Петр";
-        cat2. color = "Белый";
-        cat2. sterilization = false;
+        Laptop laptop2 = new Laptop();
+        laptop2. ram = 16;
+        laptop2. storageSize = 1024;
+        laptop2. os = "MacOS";
+        laptop2. color = "Space Gray";
 
-        Cat cat3 = new Cat();
-        cat3. name = "Толик";
-        cat3. age = 8;
-        cat3. owner = "Татьяна";
-        cat3. color = "Черный";
-        cat3. sterilization = true;
+        Laptop laptop3 = new Laptop();
+        laptop3. ram = 8; 
+        laptop3. storageSize = 256;
+        laptop3. os = "Windows";
+        laptop3. color = "Black";
 
-        Cat cat4 = new Cat();
-        cat4. name = "Чижик";
-        cat4. age = 11;
-        cat4. owner = "Алина";
-        cat4. color = "Рыжий";
-        cat4. sterilization = true;
+        Laptop laptop4 = new Laptop();
+        laptop4. ram = 16;
+        laptop4. storageSize = 1024;
+        laptop4. os = "Windows";
+        laptop4. color = "White";
 
-        Cat cat5 = new Cat();
-        cat5. name = "Чижик";
-        cat5. age = 11;
-        cat5. owner = "Алина";
-        cat5. color = "Рыжий";
-        cat5. sterilization = true;
+        Laptop laptop5 = new Laptop();
+        laptop5. ram = 8;
+        laptop5. storageSize = 256;
+        laptop5. os = "Linux";
+        laptop5. color = "Black";
 
-        Set<Cat> cats = new HashSet<>();
-        cats.add(cat1);
-        cats.add(cat2);
-        cats.add(cat3);
-        cats.add(cat4);
-        cats.add(cat5);
+        Set<Laptop> laptops = new HashSet<>();
+        laptops.add(laptop1);
+        laptops.add(laptop2);
+        laptops.add(laptop3);
+        laptops.add(laptop4);
+        laptops.add(laptop5);
 
-        printSet(cats);
+        printSet(laptops);
 
         System.out.println();
-        printSet(findByColor("Белый", cats));
+        printSet(findByColor("Белый", laptops));
     }
 
 // Ищем котика по нужному цвету
-    static Set<Cat> findByColor(String color, Set<Cat> cats){
-        Set<Cat> res = new HashSet<>();
-        for (Cat cat:cats){
-            if (cat.color.equals(color)){
+    static Set<Laptop> findByColor(String color, Set<Laptop> laptops){
+        Set<Laptop> res = new HashSet<>();
+        for (Laptop laptop:laptops){
+            if (laptop.color.equals(color)){
 
             }
         }
@@ -63,9 +74,17 @@ public class Main {
     } 
 
 // Создаем множество в котором будем хранить экземпляры класса Set
-    static void printSet(Set<Cat> cats){
-        for (Cat cat: cats){
-            System.out.println(cat);
+    static void printSet(Set<Laptop> laptops){
+        for (Laptop laptop: laptops){
+            System.out.println(laptop);
         }
     }
+
+    // Scanner scanner = new Scanner(System.in);
+    // System.out.println("Ввод вопроса");
+    // String answer = scanner.nextLine();
+    // while (!answer.equals("5")){
+
+    // }
+
 }
